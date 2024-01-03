67f946faf6e15e5a3e7f6576138292eba312d3b3
mariashubna <mariashubna@gmail.com>
2024-01-03 11:10:00 +0100

    /

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+class Key {
+    private signature: number;
+    constructor() {
+      this.signature = Math.random();
+    }
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+
+
+  class Person {
+    private key: Key;
+    constructor(key: Key) {
+      this.key = key;
+    }
+      getKey(): Key {
+      return this.key;
+    }
+  }
+
+  abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+
+  class MyHouse extends House {
+    constructor(key: Key) {
+      super(key);
+    }
+  
+    openDoor(key: Key): void {
+      if (this.key.getSignature() === key.getSignature()) {
+        this.door = true;
+      }
+    }
+  }
+  
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
